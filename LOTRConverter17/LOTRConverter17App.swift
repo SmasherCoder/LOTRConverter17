5d7e19093ec3a87d361f36df1f73e5d0477c4699
smashercoder <smasher.coder@gmail.com>
2024-07-26 10:45:45 -0500

    Main screen complete

---
+++ LOTRConverter17/LOTRConverter17App.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverter17App.swift
+//  LOTRConverter17
+//
+//  Created by Travis Helms on 7/23/24.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverter17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
