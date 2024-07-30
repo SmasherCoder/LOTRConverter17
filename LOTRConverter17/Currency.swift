4872d0b7ce5b470057b8c4a663b7c451f7cd5ef2
smashercoder <smasher.coder@gmail.com>
2024-07-30 12:03:58 -0500

    added values to currencies by adding a enum

---
+++ LOTRConverter17/Currency.swift
@@ -0,0 +1,14 @@
+//
+//  Currency.swift
+//  LOTRConverter17
+//
+//  Created by Travis Helms on 7/30/24.
+//
+
+enum Currency: Double {
+    case copperPenny = 6400
+    case silverPenny = 64
+    case silverPiece = 16
+    case goldPenny = 4
+    case goldPiece = 1
+}
