928ca07558b747bbf6fbd9e5d98152604c54d3d1
smashercoder <smasher.coder@gmail.com>
2024-07-31 00:07:23 -0500

    Added currency amounts and linked them to images

--- LOTRConverter17/Currency.swift
+++ LOTRConverter17/Currency.swift
@@ -5,10 +5,43 @@
 //  Created by Travis Helms on 7/30/24.
 //
 
+import SwiftUI
+
 enum Currency: Double {
     case copperPenny = 6400
     case silverPenny = 64
     case silverPiece = 16
     case goldPenny = 4
     case goldPiece = 1
+    
+    var image: ImageResource {
+        switch self {
+        case .copperPenny:
+                .copperpenny
+        case .silverPenny:
+                .silverpenny
+        case .silverPiece:
+                .silverpiece
+        case .goldPenny:
+                .goldpenny
+        case .goldPiece:
+                .goldpiece
+        }
+    }
+    
+    var name: String {
+        switch self {
+        case .copperPenny:
+            "Copper Penny"
+        case .silverPenny:
+            "Silver Penny"
+        case .silverPiece:
+            "Silver Piece"
+        case .goldPenny:
+            "Gold Penny"
+        case .goldPiece:
+            "Gold Piece"
+        }
+    }
 }
+
